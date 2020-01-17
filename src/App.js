ae76384bb964fc7a82c037e15d0e89ce3d067c3e
josephdecena <decenaj@uw.edu>
2020-01-17 10:39:12 -0800

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  return <main>
+  
+  <header> 
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+  <img alt = "icon" className="logo"
+    src="https://www.stickpng.com/assets/images/5852cd8058215f0354495f6b.png"
+    />
+    Chatter
+  </header>
+
+  <TextInput onSend = {t=> console.log(t)}/>
+ </main>
+
 }
 
+function TextInput(props) {
+  var [text, setText] = useState('')
+
+  return <div className = "text-input">
+  <input value ={text}
+    placeholder="Message..."
+    onChange = {e=> setText(e.target.value)}
+  />
+
+  <button onClick = {() => {
+    props.onSend(text)
+    setText('send')
+  }} > 
+  <i class = 'fas fa-angle-right'></i>
+  </button>
+  </div>
+}
 export default App;
